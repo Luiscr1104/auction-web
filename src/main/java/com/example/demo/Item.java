6ea70ae5a450fd6d4a347c3315aa5522c8bd836f
Luis R Zunniga S <luiscr1104@gmail.com>
2022-01-08 15:40:53 -0600

    Primeras clases

---
+++ src/main/java/com/example/demo/Item.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class Item {
+	String Name = "";
+
+	public static void main(String[] args) {
+		String[] itemList = new String[3];
+		itemList[0] = "Bicycle";
+		itemList[1] = "Scooter";
+		itemList[2] = "Boat";
+		System.out.println(itemList[1]);
+	}
+}
